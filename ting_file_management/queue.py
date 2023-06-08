26870c0e02c7deb835ac8a21b454ad938e05ae1f
Marcel Albuquerque <msalbuquerque@hotmail.com>
2023-06-08 17:05:25 -0300

    feat: req-01 - fila implementada

--- ting_file_management/queue.py
+++ ting_file_management/queue.py
@@ -3,16 +3,24 @@ from ting_file_management.abstract_queue import AbstractQueue
 
 class Queue(AbstractQueue):
     def __init__(self):
-        """Req 01 - Inicialize sua estrutura aqui"""
+        self._queue = list()
 
     def __len__(self):
-        """Aqui irá sua implementação"""
+        # quando pedido o tamanho do array
+        # retonar o tamanho da fila
+        return len(self._queue)
 
     def enqueue(self, value):
-        """Aqui irá sua implementação"""
+        return self._queue.append(value)
 
     def dequeue(self):
-        """Aqui irá sua implementação"""
+        # se não houver mais itens na fila, o método retornará None
+        if len(self._queue) == 0:
+            return None
+        # o método pop removerá e retornará o valor do índice fornecido
+        return self._queue.pop(0)
 
     def search(self, index):
-        """Aqui irá sua implementação"""
+        if index < 0 or index >= len(self._queue):
+            raise IndexError("Índice Inválido ou Inexistente")
+        return self._queue[index]
