6ee6cd760a5d70fdfde3fb9052fcd51ee0a8ad68
Marcel Albuquerque <msalbuquerque@hotmail.com>
2023-06-11 11:50:20 -0300

    feat: req-03, função process implementada

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,4 +1,20 @@
+import sys
+from ting_file_management.file_management import txt_importer
+
+
 def process(path_file, instance):
+    file = txt_importer(path_file)
+    data = {
+        "nome_do_arquivo": path_file,
+        "qtd_linhas": len(file),
+        "linhas_do_arquivo": file
+    }
+    for i in instance._queue:
+        if i["nome_do_arquivo"] != path_file:
+            instance.enqueue(data)
+    if len(instance._queue) == 0:
+        instance.enqueue(data)
+    print(data, file=sys.stdout)
     """Aqui irá sua implementação"""
 
 
