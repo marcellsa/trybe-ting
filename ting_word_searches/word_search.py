d27abe2398e20cf33b59e1bf643cec47a5e6e4de
Marcel Albuquerque <msalbuquerque@hotmail.com>
2023-06-15 17:42:04 -0300

    feat: req-07, função exists_word implementada

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,5 +1,19 @@
 def exists_word(word, instance):
-    """Aqui irá sua implementação"""
+    result = []
+
+    for i in range(instance.__len__()):
+        file = instance.search(i)
+        data = {
+            "palavra": word,
+            "arquivo": file["nome_do_arquivo"],
+            "ocorrencias": []
+        }
+        for j, line in enumerate(file["linhas_do_arquivo"]):
+            if word.lower() in line.lower():
+                data["ocorrencias"].append({"linha": j + 1})
+        if len(data["ocorrencias"]) > 0:
+            result.append(data)
+    return result
 
 
 def search_by_word(word, instance):
